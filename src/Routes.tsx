0938d909f4e268271ad4b6fb4a592d5c7f85a6f0
chaiyaphat <chaiyaphat.b5922314@example.com>
2021-06-04 13:57:01 +0700

    feat: register

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,19 @@
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import User from './pages/detail/User'
+import Register from './pages/register/Register'
 function Routes() {
   return (
-    <div>
-      <User />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -6,15 +6,15 @@ import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 import { css } from '@emotion/react'
-import { HashLoader
-} from 'react-spinners'
+import { HashLoader } from 'react-spinners'
 
 import * as yup from 'yup'
 import { useHistory } from 'react-router-dom'
 
 interface IFormInputs {
   email: string
-  password: string
+  password: string,
+  confirmPassword:string
 }
 
 const override = css`
@@ -26,10 +26,10 @@ const override = css`
 const schema = yup.object().shape({
   email: yup.string().required('กรุณากรอกอีเมลล์').email('รูปแบบอีเมลล์ไม่ถูกต้อง'),
   password: yup.string().required('กรุณากรอกรหัสผ่าน').min(6, 'รหัสผ่าน 6 ตัวขึ้นไป'),
+  confirmPassword:yup.string().required('กรุณากรอกรหัสผ่าน').min(6, 'รหัสผ่าน 6 ตัวขึ้นไป'),
 })
 
-
-function Login() {
+function Register() {
   let [loading, setLoading] = useState(true)
   let [color, setColor] = useState('#d38258')
   const history = useHistory()
@@ -48,7 +48,7 @@ function Login() {
     })
   }
 
-  const handleRegister = (e:MouseEvent<HTMLButtonElement>) =>{
+  const handleRegister = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
     // setLoading(!loading)
     history.replace('/register')
@@ -57,7 +57,7 @@ function Login() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen ">
       <div className="bg-white w-mobile md:w-tablet h-custom1 rounded-xl">
-        <h1 className="text-3xl text-center text-sepia-800 mt-14">QSoft Tracking</h1>
+        <h1 className="text-3xl text-center text-sepia-800 mt-14">Sing Up</h1>
         <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded-xl" onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-4">
             <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="username">
@@ -79,30 +79,31 @@ function Login() {
             />
             <p className="text-xs italic text-red-500">{errors.password?.message}</p>
           </div>
-          <div className="flex items-end justify-between mb-6">
-            <label className="inline-flex items-center mt-3">
-              <input type="checkbox" className="w-5 h-5 text-gray-600 form-checkbox" defaultChecked />
-              <span className="ml-2 text-gray-700">Stay Connected</span>
+          <div className="mb-1">
+            <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="password">
+              Confirm Password
             </label>
-            <a className="inline-block text-sm font-bold text-blue-500 align-baseline hover:text-blue-800" href="#">
-              Forgot Password?
-            </a>
+            <input
+              {...register('confirmPassword')}
+              className="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="******************"
+            />
+            <p className="text-xs italic text-red-500">{errors.password?.message}</p>
           </div>
-          <div className="flex flex-col items-center mb-8">
-            <button onClick={notify} className="w-full px-4 py-2 font-bold text-white rounded bg-sepia-500 hover:bg-sepia-700 focus:outline-none focus:shadow-outline" type="submit">
-              Login
-            </button>
-            <h1 className="py-2">Or</h1>
-            <button onClick={handleRegister} className="w-full px-4 py-2 font-bold text-white rounded bg-sepia-500 hover:bg-sepia-700 focus:outline-none focus:shadow-outline" type="button">
-              Register
-            </button>
-          </div>
-          <HashLoader color={loading?color:"white"}  css={override} size={20} />
+          <button  className="w-full px-4 py-2 mb-8 font-bold text-white rounded bg-sepia-500 hover:bg-sepia-700 focus:outline-none focus:shadow-outline" type="submit">
+            Submit
+          </button>
+          <button onClick={() => history.replace('/')} className="w-full px-4 py-2 mb-8 font-bold text-white rounded bg-sepia-500 hover:bg-sepia-700 focus:outline-none focus:shadow-outline" type="submit">
+            Back
+          </button>
+          <HashLoader color={loading ? color : 'white'} css={override} size={20} />
         </form>
-        <ToastContainer  position="top-center" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+        <ToastContainer position="top-center" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
       </div>
     </div>
   )
 }
 
-export default Login
+export default Register
