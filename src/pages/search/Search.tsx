1bbc17b7c643f59213a04e00a0d66cfcb80b42d2
chaiyaphat <chaiyaphat.b5922314@example.com>
2021-06-06 14:14:31 +0700

    feat: add button search

--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -1,4 +1,4 @@
-import { FormControl, InputLabel, MenuItem, Select } from '@material-ui/core'
+import { FormControl, InputLabel, MenuItem, Select, TextField } from '@material-ui/core'
 function Search() {
   return (
     <div className="flex items-center justify-center h-screen md:flex-col ">
@@ -6,26 +6,31 @@ function Search() {
         <h1 className="text-white md:text-xl font-Prompt">Welcome Chaiyaphat supharak</h1>
         <h1 className="text-white md:text-2xl md:font-bold">QSOFTTRACK && TRAC</h1>
       </div>
-      <div className="flex flex-col md:justify-center md:flex-row bg-gray-50 w-custom1 h-custom2">
+      <div className="flex flex-col items-center justify-start md:justify-start md:items-center bg-gray-50 w-custom1 h-custom2 rounded-xl">
+        <div className="w-1 h-7" />
         <h1 className="text-4xl text-center md:hidden text-sepia-800">QSoft Tracking</h1>
-
-        <div className="flex items-center justify-center w-full h-14 md:w-10/12 bg-sepia-200">
-        <FormControl variant="outlined" size="small">
-          <InputLabel id="labelId">Company</InputLabel>
-          <Select labelId="selectId" id="selectId" label="Company">
-            <MenuItem value="">
-              <h1>โปรดระบุบริษัท</h1>
-            </MenuItem>
-            <MenuItem value={'id1'}>
-              <h1  className="text-xs md:text-xl xl:text-2xl font-Prompt">บริษัท เกษตรไทย อินเตอร์เนชั่นแนล ชูการ์ คอร์ปอเรชั่น1</h1>
-            </MenuItem>
-            <MenuItem value={'id1'}>
-              <h1  className="text-xs md:text-xl xl:text-2xl">บริษัท เกษตรไทย อินเตอร์เนชั่นแนล ชูการ์ คอร์ปอเรชั่น1</h1>
-            </MenuItem>
-          </Select>
-        </FormControl>
+        <div className="h-7" />
+        <div className="flex flex-col items-center justify-center w-10/12 ">
+          <FormControl variant="outlined" size="small" className="w-full">
+            <InputLabel>Company</InputLabel>
+            <Select labelId="selectId" id="selectId" label="Company">
+              <MenuItem value="">
+                <h1>โปรดระบุบริษัท</h1>
+              </MenuItem>
+              <MenuItem value={'id1'}>
+                <h1 className="text-xs md:text-lg font-Prompt">บริษัท เกษตรไทย อินเตอร์เนชั่นแนล ชูการ์ คอร์ปอเรชั่น1</h1>
+              </MenuItem>
+              <MenuItem value={'id1'}>
+                <h1 className="text-xs md:text-lg ">บริษัท เกษตรไทย อินเตอร์เนชั่นแนล ชูการ์ คอร์ปอเรชั่น1</h1>
+              </MenuItem>
+            </Select>
+          </FormControl>
+          <div className="h-7" />
+          <TextField placeholder="TRACKING ID" InputLabelProps={{ shrink: false }} className="w-full " id="outlined-multiline-static" multiline rows={8} variant="outlined" />
+          <button className="self-end w-40 px-4 py-2 font-bold text-white rounded mt-7 bg-sepia-500 hover:bg-sepia-700 focus:outline-none focus:shadow-outline" type="submit">
+            Search
+          </button>
         </div>
-
       </div>
     </div>
   )
